90921ea751d18676249310ed9c9b6127bda1f3f2
Cal <steppinkurry@gmail.com>
2023-07-10 20:39:22 -0400

    I forget how much I changed since the last commit, so here's a new one. I *think* I changed hit detection to work a little better in this one. That's the only real change I can think of so far. More to come soon. I hope.

--- crane.c
+++ crane.c
@@ -14,7 +14,8 @@ int main(int argc, char *argv[])
 	struct graphics crap;
 	graphics_init(&crap, WINDOW_X, WINDOW_Y);
 		
-	float scroll_speed = 0.2f;
+	float ground_speed = 0.2f;
+	float air_speed = 0.05f;
 	
 	// Player box struct
 	SDL_Rect pbox;
@@ -28,8 +29,9 @@ int main(int argc, char *argv[])
 	struct xyf playerv; // Player velocity
 
 	char grounded = 0;
+	char wall_jumpable = 0;
 
-
+	int count = 0;
 
 	// MAP
 	int map[100][180];
@@ -72,7 +74,6 @@ int main(int argc, char *argv[])
 		// Draws the map
 		draw_map(map, crap.renderer);
 
-
 		// Draw player box
 		SDL_SetRenderDrawColor(crap.renderer, 0, 255, 0, 100);
 		SDL_RenderFillRect(crap.renderer, &pbox);
@@ -84,55 +85,69 @@ int main(int argc, char *argv[])
 		if (input_store[0] == 1)
 		{
 			crap.window = NULL;
-			//crap_quit(&crap);
 		}
 
 		playerv.x = 0;
 
 		// Define gravity constant
-		float gravity_constant = 0.0002f;
+		float gravity_constant = 0.0002f; // 0.0002f
 
 		// Movement
-		if (input_store[5] == 1 && grounded) // Jump
-		{
-			playerv.y = -0.3;
+		if (input_store[5] == 1) // Jump
+		{	
+			// regular jump
+			if (grounded == 1) { playerv.y = -0.3; pbox.y -= 2; }
+
+			// wall jump
+			else if (wall_jumpable == 1)
+			{
+				playerv.y = -0.2;
+				
+				if (input_store[3] == 1) { playerv.x += 100; }
+				else { playerv.x -= 100; }
+				//printf("wall jump\n");
+			}
+
 		}
 		else if (input_store[2] == 1) // Fast fall
 		{
-			playerv.y = gravity_constant * 9999;
+			playerv.y = gravity_constant * 999;
 		}
 		if (input_store[3] == 1) // Left
 		{
-			playerv.x -= scroll_speed;
+			if (grounded == 1) { playerv.x -= ground_speed; }
+			else { playerv.x -= air_speed; }
 		}
 		else if (input_store[4] == 1) // Right
 		{
-			playerv.x += scroll_speed;
+			if (grounded == 1) { playerv.x += ground_speed; }
+			else { playerv.x += air_speed; }
+
 		}
 		
 		// Apply gravity
-		
 		playerv.y += gravity_constant;
-
 
 		// Physics calcs
 		
-		// Convert player's precise location to the 
-		// player's position in the map matrix
-		int pvm_x = (int) (p_pos.x + playerv.x) / 5; 
-		int pvm_y = (int) (p_pos.y + playerv.y + pbox.h) / 5;
 
 		// Add all phys rects to an array to make checking easier
 		struct phys_rect* hit_boxes[4];
 
-		// This box has v
+		// This box has velocity
 		struct phys_rect ppbox;
 		ppbox.x1 = pbox.x + playerv.x; ppbox.x2 = pbox.x + pbox.w + playerv.x;
 		ppbox.y1 = pbox.y + playerv.y; ppbox.y2 = pbox.y + pbox.h + playerv.y;
 
-		struct phys_rect ppbox_project;
-		ppbox_project.x1 = ppbox.x1 + playerv.x; ppbox_project.x2 = ppbox.x2 + playerv.x;
-		ppbox_project.y1 = ppbox.y1 + playerv.y; ppbox_project.y2 = ppbox.y2 + playerv.y;
+		// Changes to ppbox in the x direction
+		struct phys_rect ppbox_x;
+		ppbox_x.x1 = pbox.x + playerv.x; ppbox_x.x2 = pbox.x + pbox.w + playerv.x;
+		ppbox_x.y1= pbox.y; ppbox_x.y2 = pbox.y + pbox.h;
+
+		// Changes to ppbox in the y direction
+		struct phys_rect ppbox_y;
+		ppbox_y.x1 = pbox.x; ppbox.x2 = pbox.x + pbox.w;
+		ppbox_y.y1 = pbox.y + playerv.y; ppbox_y.y2 = pbox.y + pbox.h + playerv.y + 2;
 
 		struct phys_rect ground;
 		ground.x1 = 0; ground.x2 = 900;
@@ -152,62 +167,63 @@ int main(int argc, char *argv[])
 		struct phys_rect pform;
 		pform.x1 = 0; pform.x2 = 200;
 		pform.y1 = 400; pform.y2 = 394;
-		hit_boxes[3] = &pform;		
+		hit_boxes[3] = &pform;	
 
-		if (check_collision_two_dim(ppbox, ground) || check_collision_two_dim(ppbox, pform))
+		grounded = 0;
+		wall_jumpable = 0;
+
+		// Check collisions in the x direction
+		if (check_collision_two_dim(ppbox_x, ground) || check_collision_two_dim(ppbox_x, pform))
+		{
+			playerv.x = 0;
+		}
+
+		if (check_collision_two_dim(ppbox_x, lwall) || check_collision_two_dim(ppbox_x, rwall))
+		{
+			playerv.x = 0;
+			wall_jumpable = 1;
+		}
+
+		// Check collisions in the y direction
+		if (check_collision_two_dim(ppbox_y, ground))
 		{
 			grounded = 1;
 			playerv.y = 0;
-		}
-		else
-		{
-			//printf("this is happenenning\n");
-			p_pos.y += playerv.y;
-			grounded = 0;
+			p_pos.y = ground.y1 - (pbox.h + 1);
 		}
 
-		if (check_collision_two_dim(ppbox, lwall) || check_collision_two_dim(ppbox, rwall))// || check_collision_two_dim(ppbox, pform))
+		if (check_collision_two_dim(ppbox_y, pform))
 		{
-			//printf("Collisison\n");
+			playerv.y = 0;
+
+			// hit the platform from above
+			if (ppbox_y.y1 < pform.y2) 
+			{
+				p_pos.y = pform.y2 - (pbox.h - 5); 
+				grounded = 1;
+			}
+
+			// hit the platform from below
+			else if (ppbox_y.y2 > pform.y1) 
+			{
+				p_pos.y = pform.y1 + 3; 
+			}
+			
 		}
-		else
-		{
-			p_pos.x += playerv.x;
-		}
-
-		/*
-		// Check x axis collision
-		if (map[(int)p_pos.y / 5][pvm_x] != 0)
-		{
-			// Pretty much just do nothing
-		}
-		else 
-		{
-			p_pos.x += playerv.x;
-		}
-
-		// Check y axis collision
-		if (map[pvm_y][(int)p_pos.x / 5] != 0)
-		{
-			grounded = 1;
-		}
-
-		else
-		{
-			p_pos.y += playerv.y;
-			grounded = 0;
-
-		}*/
+		
+		//printf("%d: grounded?: %d\n", count, grounded);
 
 
+		p_pos.x += playerv.x;
+		p_pos.y += playerv.y;
 
 		pbox.x = (int) p_pos.x;
 		pbox.y = (int) p_pos.y;
 
-
-
 		// Draw to screen
 		SDL_RenderPresent(crap.renderer);
+
+		count += 1;
 
 	}
 
